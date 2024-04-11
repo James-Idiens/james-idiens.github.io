9aae6f94e64291641dfe39c0ef444554f5ddf6eb
james <jidiens@outlook.com>
2024-04-12 10:51:07 +1200

    Update Astro configuration and remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,38 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "LocalMaps",
+      logo: {
+        src: "./src/assets/localmaps-logo.png", // Path to your logo image
+        alt: "Alternative text for logo", // Alternative text for accessibility
+        replacesTitle: true, // Set to true if you want to hide the site title text and only show the logo
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Admin Guide",
+          collapsed: true,
+          // the autogenerate option will automatically generate a sidebar based on the files in the directory
+          autogenerate: { directory: "Admin-Guide" },
+        },
+        {
+          label: "User Guide",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Getting Started", link: "user-guide/user-guide" },
+            { label: "Gallery", link: "user-guide/gallery" },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
---
+++ src/content/docs/User-Guide/gallery.md
@@ -0,0 +1,21 @@
+---
+title: Gallery
+description: User guide to the gallery
+---
+
+The LocalMaps Gallery is your central mapping destination! This page outlines how to navigate the Gallery to find what you are looking for.
+
+## Navigating around the gallery
+
+1. **Search** - Search for content by title or keyword by using the top right search bar.
+2. **Categories** - Filter gallery content down to a defined subgroup using the left hand side Categories. 
+3. **Sort by & layout** - Change the sorting of the gallery by Most Views, Title or Most recent. Optionally change the gallery layout to listed or as gallery cards (default)
+4. **About LocalMaps** - Read about the site and how to use it
+5. **Log in** - Log into LocalMaps using an ArcGIS Login (to access secure content)
+6. **Content Card** - View maps and other spatial content by clicking on a card
+7. **Metadata** - View item information about each application or map
+8. **Secure Content** - Secure items are identified with a small lock symbol. These are only available once logged in
+
+![Gallery Guide](/src/assets/gallery-guide.png)
+
+**Additional Links** -  Any additional links and information can be accessed in the bottom footer.
