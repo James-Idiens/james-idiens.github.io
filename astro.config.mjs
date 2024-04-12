5bb2dcc7bdd2561a22938860ffb947037ed41bd4
james <jidiens@outlook.com>
2024-04-12 12:16:43 +1200

    before deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  // site: 'https://james-idiens.github.io',
+
   integrations: [
     starlight({
       title: "LocalMaps",
@@ -12,26 +14,29 @@ export default defineConfig({
         alt: "Alternative text for logo", // Alternative text for accessibility
         replacesTitle: true, // Set to true if you want to hide the site title text and only show the logo
       },
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
+      // social: {
+      //   github: "https://github.com/withastro/starlight",
+      // },
       sidebar: [
         {
           label: "Admin Guide",
           collapsed: true,
-          // the autogenerate option will automatically generate a sidebar based on the files in the directory
-          autogenerate: { directory: "Admin-Guide" },
+          items: [
+            { label: "Getting Started", link: "admin-guide/introduction" },
+          ],
         },
         {
           label: "User Guide",
+          collapsed: true,
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Getting Started", link: "user-guide/user-guide" },
+            { label: "Getting Started", link: "user-guide/introduction" },
             { label: "Gallery", link: "user-guide/gallery" },
           ],
         },
       ],
       customCss: ["./src/tailwind.css"],
+      favicon: "/favicon.png",
     }),
     tailwind({ applyBaseStyles: false }),
   ],
---
+++ src/content/docs/Admin-Guide/introduction.md
@@ -0,0 +1,23 @@
+---
+title: Admin Guide
+description: Introduction to the Admin Guide
+---
+
+
+# Welcome
+
+Kia ora, and welcome to the Administration Guide for LocalMaps 3.x.
+
+This guide is designed for users of all skill levels, to familiarise themselves with LocalMaps 3.x and how to configure specific elements to make the most of all the capabilities. Here you will find basic navigation advice, how to use different tools, how to access maps and content, steps to configure themes, and how to learn more about the data, layers, and information.
+
+If your organisation is brand new to LocalMaps, check out the About LocalMaps section for information on what's included, what you need, and how to get started with LocalMaps. If you have any suggestions or feedback for LocalMaps, you can reach out to our support line at localmapssupport@eagle.co.nz.
+
+## How to use the Administration Guide
+
+You can visit each section in the side menu to make the most of this help guide.
+
+Looking for something specific? Use the searchbar above
+
+---
+
+Thank you for choosing our Local Maps website. Let's get started!
